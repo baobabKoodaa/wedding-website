955aa2a29a63768a209df3d2a05680d3a9220ec2
Atte Juvonen <atte.juvonen@futurice.com>
2019-01-12 02:17:21 +0200

    Minor tweak

--- js/scripts.js
+++ js/scripts.js
@@ -219,6 +219,7 @@ function showRSVPYesTree() {
 function showRSVPNoTree() {
     $("#rsvp-root").hide();
     $("#rsvp-no").show();
+    $("#rsvp-wishes").hide();
     $("#rsvp-submit").text('En valitettavasti pääse paikalle');
     $("#rsvp-submit").css({ "background-color": '#F0F0F0' , "border-color": '#F0F0F0' , 'color':'rgba(28,54,83,0.6)'});
     $("#rsvp-end").show();
